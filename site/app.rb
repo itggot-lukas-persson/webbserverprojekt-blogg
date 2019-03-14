2c801ccf19106ee7af32fa6989c6e62ca0f141fb
itggot-lukas-persson <lukas.persson@itggot.se>
2019-03-14 10:56:41 +0100

    tried to fix login

--- site/app.rb
+++ site/app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+enable :sessions
 
 get('/') do
     slim(:index)
@@ -21,22 +22,20 @@ end
 
 post('/register/new') do
     db = SQLite3::Database.new("db/blogg.db")
-    hash_passsword = BCrypt::Password.create(params["password"])
-    db.execute('INSERT INTO User(username, password) VALUES ((?), (?))', params["username"], hash_passsword)
+    hash_passsword = BCrypt::Password.create(params[:password])
+    db.execute('INSERT INTO User(username, password) VALUES ((?), (?))', params[:username], hash_passsword)
     redirect('/') 
 end
 
 post('/login') do
     db = SQLite3::Database.new("db/blogg.db")
     db.results_as_hash = true
-
-    result = db.execute("SELECT username, password FROM User WHERE User.username=(?)",params["username"], params["password"])
-    new = result[0]
-    if params["username"] == new[0] && BCrypt::Password.new(hash_passsword) == new[1]
+    result = db.execute("SELECT username, password FROM User WHERE User.username=(?)",params[:username])
+    
+    if BCrypt::Password.new(hash_passsword) == params[:password]
         redirect('/logged')
     else 
         redirect('/opps')
     end
 end 
-
 
